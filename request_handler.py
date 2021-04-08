dc3cb8f762e4e861e1e5a6839b63a27d9f638dbf
Alex Martin <alex@subtleco.co>
2021-04-08 15:45:57 -0500

    post location

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
-from locations import get_single_location, get_all_locations
+from locations import get_single_location, get_all_locations, create_location
 from employees import get_single_employee, get_all_employees
 from customers import get_single_customer, get_all_customers
 import json
@@ -98,12 +98,14 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         resource, id = self.parse_url(self.path)
 
-        new_animal = None
+        new_item = None
 
         if resource == "animals":
-            new_animal = create_animal(post_body)
+            new_item = create_animal(post_body)
+        elif resource == "locations":
+            new_item = create_location(post_body)
 
-        self.wfile.write(f"{new_animal}".encode())
+        self.wfile.write(f"{new_item}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
