e98bd84961d54dd240da47361c9da4d3f14b3610
Alex Martin <alex@subtleco.co>
2021-04-13 15:10:50 -0500

    employees by location:

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer, ThreadingHTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animals_by_location
 from customers import get_single_customer, get_all_customers, create_customer, delete_customer, update_customer, get_customer_by_email
-from employees import get_single_employee, get_all_employees, create_employee, delete_employee, update_employee
+from employees import get_single_employee, get_all_employees, create_employee, delete_employee, update_employee, get_employees_by_location
 from locations import get_single_location, get_all_locations, create_location, delete_location, update_location
 import json
 
@@ -102,6 +102,9 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             if key == "location_id" and resource == "animals":
                 response = f"{get_animals_by_location(value)}"
+
+            if key == "location_id" and resource == "employees":
+                response = f"{get_employees_by_location(value)}"
 
         # This weird code sends a response back to the client
         self.wfile.write(f"{response}".encode())
