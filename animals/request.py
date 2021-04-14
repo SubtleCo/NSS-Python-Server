7ec0ed21422ea19fc1995fc25148dbe31f38a822
Alex Martin <alex@subtleco.co>
2021-04-14 13:39:52 -0500

    Animals now able to PUT

--- animals/request.py
+++ animals/request.py
@@ -72,12 +72,14 @@ def get_all_animals():
     return json.dumps(animals)
 
 # Function with a single parameter
+
+
 def get_single_animal(id):
     with sqlite3.connect("./kennel.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        db_cursor.execute( """
+        db_cursor.execute("""
         SELECT
             a.id,
             a.name,
@@ -92,16 +94,17 @@ def get_single_animal(id):
         data = db_cursor.fetchone()
 
         animal = Animal(data['id'], data['name'], data['breed'],
-                            data['status'], data['location_id'],
-                            data['customer_id'])
+                        data['status'], data['location_id'],
+                        data['customer_id'])
         return json.dumps(animal.__dict__)
+
 
 def get_animals_by_location(location_id):
     with sqlite3.connect("./kennel.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        db_cursor.execute( """
+        db_cursor.execute("""
         SELECT
             a.id,
             a.name,
@@ -125,12 +128,13 @@ def get_animals_by_location(location_id):
     # Use `json` package to properly serialize list as JSON
     return json.dumps(animals)
 
+
 def get_animals_by_status(status):
     with sqlite3.connect("./kennel.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        db_cursor.execute( """
+        db_cursor.execute("""
         SELECT
             a.id,
             a.name,
@@ -166,6 +170,7 @@ def create_animal(animal):
 
     return animal
 
+
 def delete_animal(id):
     with sqlite3.connect("./kennel.db") as conn:
         db_cursor = conn.cursor()
@@ -175,8 +180,30 @@ def delete_animal(id):
         WHERE id = ?
         """, (id, ))
 
+
 def update_animal(id, new_animal):
-    for index, animal in enumerate(ANIMALS):
-        if animal["id"] == id:
-            ANIMALS[index] = new_animal
-            break
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE animal
+            SET
+                name = ?,
+                breed = ?,
+                status = ?,
+                location_id = ?,
+                customer_id = ?
+        WHERE id = ?
+        """, (new_animal['name'],
+              new_animal['breed'],
+              new_animal['status'],
+              new_animal['location_id'],
+              new_animal['customer_id'],
+              id
+              ))
+        
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        return False
+    else: return True
